2a69d35fa3c77953d842c4e8cc6db0a9c19b9183
qp0kycHuk <serega990112@mail.ru>
2024-10-24 15:00:53 +0300

    change header footer

--- src/js/components/index.ts
+++ src/js/components/index.ts
@@ -1,9 +1,11 @@
 import counter from './counter'
 import icon from './icon'
+import menu from '@qpokychuk/wc-menu'
 
 function registerAll() {
   counter.register()
   icon.register()
+  menu.register()
 }
 
 registerAll()
